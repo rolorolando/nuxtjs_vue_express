9ec378a592a06d4440d9172c384f6739bb0dfc80
rolorolando <80335416+rolorolando@users.noreply.github.com>
2021-04-06 12:09:14 -0400

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,10 +68,10 @@ export default {
   ** For deployment you might want to edit host and port
   */
   
-   server: {
-      port: process.env.PORT || 5000, // default: 3000
-  //  host: '0.0.0.0' // default: localhost
-   },
+//   server: {
+//     port: process.env.PORT || 5000, // default: 3000
+//  host: '0.0.0.0' // default: localhost
+//   },
 
   /*
   ** Build configuration
